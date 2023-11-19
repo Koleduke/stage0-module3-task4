fd1605d83cd4567da0cb0c9eb7a86a20d517778e
Koleduke <ydel2014@gmail.com>
2023-11-19 18:15:41 +0300

    done

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-    }
+
+
+        int c = 99;
+        char cc = (char) c;
+        int h = 104;
+        char hh = (char) h;
+        int a = 97;
+        char aa = (char) a;
+        int r = 114;
+        char rr = (char) r;
+        System.out.print(cc);
+        System.out.print(hh);
+        System.out.print(aa);
+        System.out.print(rr);
+      }
 }
